6faee2b17a3f6a1f6ca06b1485d1a4a838177547
as12015 <Soloalby2209>
2016-05-20 19:09:07 +0100

    Add data_processing skeleton.
    Added data_processing.h, data_processing.c as skeleton files.

--- src/data_processing.h
+++ src/data_processing.h
@@ -1,11 +1,18 @@
-void dp_exec(void);
+#ifndef DATA_PROCESSING_H
+#define DATA_PROCESSING_H
+
+#include <stdio.h>
+
+void dp_exec(void*);
 
 struct dp_instr {
-	cond: 4
-	imm_op: 1
-	op_code: 4
-	set_cond: 1
-	op1: 4
-	dest: 4
-	op2: 12
-}
+	uint32_t cond: 4;
+	uint32_t imm_op: 1;
+	uint32_t op_code: 4;
+	uint32_t set_cond: 1;
+	uint32_t op1: 4;
+	uint32_t dest: 4;
+	uint32_t op2: 12;
+};
+
+#endif /*DATA_PROCESSING_H*/
