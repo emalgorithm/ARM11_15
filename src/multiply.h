7beac13f95bc7bc5a48cfecdadb6407e5849dec6
Karol Ciszek <karol.ciszek15@imperial.ac.uk>
2016-05-20 17:20:18 +0100

    Add skeleton for multiply; add to makefile
    Will merge with develop now, then work on instr_multiply branch.

---
+++ src/multiply.h
@@ -0,0 +1,18 @@
+#ifndef multiply
+#define multiply
+
+#include <stdint.h>
+
+/* Include argument name in declarations? */
+void dp_exec(void*);
+
+struct dp_instr {
+    uint32_t cond : 4;
+    uint32_t imm_op: 1;
+    uint32_t set_cond : 1;
+    uint32_t op1 : 4;
+    uint32_t dest : 4;
+    uint32_t op2 : 12;
+};
+
+#endif
