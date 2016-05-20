1e000e7e63d454cff415f63723a175a45b84a252
Karol Ciszek <karol.ciszek15@imperial.ac.uk>
2016-05-20 17:57:39 +0100

    Fix multiply.h struct and names

--- src/multiply.h
+++ src/multiply.h
@@ -4,15 +4,16 @@
 #include <stdint.h>
 
 /* Include argument name in declarations? */
-void dp_exec(void*);
+void mul_exec(void*);
 
-struct dp_instr {
+struct mul_instr {
     uint32_t cond : 4;
-    uint32_t imm_op: 1;
+    uint32_t acc: 1;
     uint32_t set_cond : 1;
-    uint32_t op1 : 4;
     uint32_t dest : 4;
-    uint32_t op2 : 12;
+    uint32_t op1 : 4;
+    uint32_t op2 : 4;
+    uint32_t op3 : 4;
 };
 
 #endif
