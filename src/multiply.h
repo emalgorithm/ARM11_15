696ffc2ca2357d3ee24fcb4b3eab295b2a094040
Karol Ciszek <karol.ciszek15@imperial.ac.uk>
2016-05-20 18:02:58 +0100

    Rename registers in mul_instr struct

--- src/multiply.h
+++ src/multiply.h
@@ -11,9 +11,9 @@ struct mul_instr {
     uint32_t acc: 1;
     uint32_t set_cond : 1;
     uint32_t dest : 4;
-    uint32_t op1 : 4;
-    uint32_t op2 : 4;
-    uint32_t op3 : 4;
+    uint32_t rn : 4;
+    uint32_t rs : 4;
+    uint32_t rm : 4;
 };
 
 #endif
