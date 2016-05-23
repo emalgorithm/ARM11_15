df3a08b9354258370621d290127623df21d119b8
as12015 <as12015@ic.ac.uk>
2016-05-23 11:02:14 +0100

    Update If-Else statements of Op_Code to support Enum Type

--- src/emulator/data_processing.c
+++ src/emulator/data_processing.c
@@ -7,21 +7,12 @@ void dp_exec (void* instruction) {
 	/*Work In Progress*/
 
 	/*Declarations*/
-    struct dp_instr dp_instruction;
-    uint32_t AND = 0;
-	uint32_t EOR = 1;
-	uint32_t SUB = 2;
-	uint32_t RSB = 3;
-	uint32_t ADD = 4;
-	uint32_t TST = 5;
-	uint32_t TEQ = 6;
-	uint32_t CMP = 7;
-	uint32_t ORR = 8;
-	uint32_t MOV = 9;
+    
+    enum instr_op_code {AND, EOR, SUB, RSB, ADD, TST, TEQ, CMP, ORR, MOV};
 
 	/*Put Value at instruction in dp_instr*/
 	
-	dp_instruction = *((struct dp_instr *) instruction);
+	struct dp_instr dp_instruction = *((struct dp_instr *) instruction);
 
 	/*Switch here for the two cases of Operand 2*/
 
@@ -29,40 +20,43 @@ void dp_exec (void* instruction) {
 	/*Different behaviours acco*/
 
 	uint32_t op_code = dp_instruction.op_code;
+	
+	switch (op_code) {
+	    case AND:
+	        printf("AND, OpCode: %d/n", op_code);
+	        break;
+        case EOR:
+            printf("EOR, OpCode: %d/n", op_code);
+            break;
+        case SUB:
+            printf("SUB, OpCode: %d/n", op_code);
+            break;
+        case RSB:
+            printf("RSB, OpCode: %d/n", op_code);
+            break;
+        case ADD:
+            printf("ADD, OpCode: %d/n", op_code);
+            break;
+        case TST:
+            printf("TST, OpCode: %d/n", op_code);
+            break;
+        case TEQ:
+            printf("TEQ, OpCode: %d/n", op_code);
+            break;
+        case CMP:
+            printf("CMP, OpCode: %d/n", op_code);
+            break;
+        case ORR:
+            printf("ORR, OpCode: %d/n", op_code);
+            break;
+        case MOV:
+            printf("MOV, OpCode: %d/n", op_code);
+            break;
+        default:
+            printf("INVALID OPCODE!/n");
+            break;
+	}
 
-	if (op_code == AND) {
-		printf("AND, OpCode: %d", AND);
-	} 
-	else if (op_code == EOR) {
-		printf("EOR, OpCode: %d", EOR);
-	}
-	else if (op_code == SUB) {
-		printf("SUB, OpCode: %d", SUB);
-	}
-	else if (op_code == RSB) {
-		printf("RSB, OpCode: %d", RSB);
-	}
-	else if (op_code == ADD) {
-		printf("ADD, OpCode: %d", ADD);
-	}
-	else if (op_code == TST) {
-		printf("TST, OpCode: %d", TST);
-	}
-	else if (op_code == TEQ) {
-		printf("TEQ, OpCode: %d", TEQ);
-	}
-	else if (op_code == CMP) {
-		printf("CMP, OpCode: %d", CMP);
-	}
-	else if (op_code == ORR) {
-		printf("ORR, OpCode: %d", ORR);
-	}
-	else if (op_code == MOV) {
-		printf("MOV, OpCode: %d", MOV);
-	}
-	else {
-		printf("Invalid OpCode!");
-	}
 }
 
 
