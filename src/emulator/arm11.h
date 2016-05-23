4e991670f72a89aa07839bf38b675b247f9bc33b
Karol Ciszek <karol.ciszek15@imperial.ac.uk>
2016-05-23 02:04:36 +0100

    Define CPSR flags in arm11
    If there is a better place to define these, the definitions can be
    moved. These seem to be necessary since the set_cond flag is common to
    all the instructions.
    
    Possible alternative:
        For all flags, define function which returns 0 <-> flag is not active

--- src/emulator/arm11.h
+++ src/emulator/arm11.h
@@ -7,6 +7,18 @@
 #define MEMORY_SIZE (1 << 16)
 #define NUM_OF_REGISTERS 17
 
+/* CPSR flags
+ * ----------
+ * Used to check conditions for certain ARM instructions. The flags are stored
+ * in register 16. For the purposes of this project, we are only concerned with
+ * the upper 4 bits of the register.
+ */
+#define CPSR_REG 16
+#define CPSR_N (1 << 31)
+#define CPSR_Z (1 << 30)
+#define CPSR_C (1 << 29)
+#define CPSR_V (1 << 28)
+
 /* Instruction types
  * -----------------
  * The 4 following structs represent the 4 instruction types supported by the
