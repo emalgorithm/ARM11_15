9dc84c88691751f24ce0ec6bfd77be2abe5e1d51
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-05-24 18:46:49 +0100

    Implement sdt (no tests written yet)

--- src/emulator/sdt.c
+++ src/emulator/sdt.c
@@ -1,5 +1,56 @@
+#include <stdbool.h>
 #include "sdt.h"
+#include "util/shift_reg.h"
 
-void sdt_exect(void* instruction) {
+static uint32_t get_scaled_mem_address(uint32_t mem_address, uint32_t offset, bool up) {
+    uint32_t scaled_mem_address = mem_address;
 
+    if (up) {
+        scaled_mem_address += offset;
+    } else {
+        scaled_mem_address -= offset;
+    }
+
+    return scaled_mem_address;
+}
+
+static void load_from_mem(uint32_t dst_reg_index, uint32_t mem_address) {
+    uint32_t value = get_word(mem_address);
+    set_register(dst_reg_index, value);
+}
+
+static void store_into_mem(uint32_t src_reg_index, uint32_t mem_address) {
+    uint32_t value = get_register(src_reg_index);
+    set_word(mem_address, value);
+}
+
+void sdt_exect(union decoded_instr *decoded) {
+    struct sdt_instr *sdt = &(decoded->sdt);
+    uint32_t offset = sdt->offset;
+    uint32_t mem_address = get_register(sdt->rn);
+
+    if (sdt->imm_off) {
+        offset = shift_reg(offset, 0);
+    }
+
+    /*
+     * The flag is set: add/subtract offset before transferring the data
+     */
+    if (sdt->index_bit) {
+        mem_address = get_scaled_mem_address(mem_address, offset, sdt->up);
+    }
+
+    if (sdt->load_store) {
+        load_from_mem(sdt->rd, mem_address);
+    } else {
+        store_into_mem(sdt->rd, mem_address);
+    }
+
+    /*
+     * The flag is not set: add/subtract offset after transferring the data
+     */
+    if (!sdt->index_bit) {
+        mem_address = get_scaled_mem_address(mem_address, offset, sdt->up);
+        sdt->rn = mem_address;
+    }
 }
