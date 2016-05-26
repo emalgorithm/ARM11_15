933eb70c26f2dc108af9d2579ad5408914e6c69e
as12015 <as12015@ic.ac.uk>
2016-05-26 16:08:27 +0100

    Define Arit Right Shift in terms of Log Right Shift

--- src/emulator/util/shift_reg.c
+++ src/emulator/util/shift_reg.c
@@ -28,10 +28,14 @@
 #define RS_LEN (4)
 #define INT_BIT (11)
 #define INT_LEN (5)
+#define MAX_BIT (31)
 
 typedef uint32_t (*shift_fun_ptr)(uint32_t, uint32_t, uint32_t);
 
+/* If FLAGS can be set, sets the C flag according to the shift operation performed */
 static void set_cflag_shift (uint32_t amount, uint32_t value, uint32_t index, uint32_t set_cond) {
+    // If a Shift has occured (amount >= 1)
+    // AND the set condition (set_cond) is enabled
     if (amount >= 1 && set_cond) {
         if (get_bit(value, index)) {
             set_cflag;
@@ -43,23 +47,30 @@ static void set_cflag_shift (uint32_t amount, uint32_t value, uint32_t index, ui
 
 /* */
 static uint32_t log_left_shift (uint32_t amount, uint32_t value, uint32_t set_cond) {
+    // C flag carries the last bit to be discarded
     set_cflag_shift(amount, value, MAX_BITS - amount, set_cond);
     return value << amount;
 }
 static uint32_t log_right_shift (uint32_t amount, uint32_t value, uint32_t set_cond) {
+    // C flag carries the last bit 'out' to the right
     set_cflag_shift(amount, value, amount - 1, set_cond);
-    return (unsigned)value >> amount;
+    return value >> amount;
 }
 static uint32_t arit_right_shift (uint32_t amount, uint32_t value, uint32_t set_cond) {
+    // C flag carries the last bit 'out' to the right
     set_cflag_shift(amount, value, amount - 1, set_cond);
-    // If negative
-    if (get_bit(value, 31)) {
-        return value >> amount | ~(log_right_shift(amount, ~0x0, 0));
+    // Perform a Logical Right Shift
+    uint32_t res = log_right_shift(amount, value, set_cond);
+    if (get_bit(value, MAX_BIT)) {
+        // If negative keep the sign
+        return res | ~(log_right_shift(amount, ~0x0, 0));
     } else {
-        return value >> amount;
+        // If positive there is no difference
+        return res;
     }
 }
 uint32_t rot_right (uint32_t amount, uint32_t value, uint32_t set_cond) {
+    // C flag carries the last rotated bit to the right
     set_cflag_shift(amount, value, amount - 1, set_cond);
     uint32_t left_shift = (int)((unsigned)value >> amount);
     uint32_t right_shift = value << (MAX_BITS - amount);
