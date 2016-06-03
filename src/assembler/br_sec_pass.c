9c6760bffa47f2bcdad2dc8d0eb3ad9051071ea2
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2016-06-03 13:37:08 +0100

    Solve problems with branch instruction, pass all the tests

--- src/assembler/br_sec_pass.c
+++ src/assembler/br_sec_pass.c
@@ -4,6 +4,9 @@
 #include "second_pass.h"
 #include "string.h"
 
+#define PIPELINE_OFFSET 8
+#define SHIFT 2
+
 void proc_br_instr(char* br_char, union decoded_instr *decoded) {
     decoded->br._id = 0x2;
     decoded->br._id2 = 0x2;
@@ -15,7 +18,7 @@ void proc_br_instr(char* br_char, union decoded_instr *decoded) {
     uint32_t label_addr = get_label_address(label);
     uint32_t curr_addr = get_curr_instr_addr();
 
-    int32_t offset = label_addr - (curr_addr + 8);
+    int32_t offset = (label_addr - (curr_addr + PIPELINE_OFFSET)) >> SHIFT;
 
     decoded->br.offset = offset;
 }
