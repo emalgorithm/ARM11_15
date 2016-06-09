b58398567cb67c6abfe16ae3fa472fc571e08238
as12015 <as12015@ic.ac.uk>
2016-06-09 22:46:01 +0100

    Change to output file

--- test/unit/disassembler/output.s
+++ test/unit/disassembler/output.s
@@ -1,16 +1,11 @@
-mov r2, #0x1
-mov r2, r2, #0x9
-ldr r0, =#0x20200000
-str r2, [r0, #0x0]
-mov r1, #0x1
-mov r1, r1, #0x3
-str r1, [r0, #0x28]
-mov r2, #0xA
-label_0:
-str r1, [r0, #0x28]
+mov r2, #0x5
+label_1:
 sub r2, r2, #0x1
-cmp r2, #0x0
-str r1, [r0, #0x1C]
+mov r1, #0xA
+label_0:
+sub r1, r1, #0x1
+cmp r1, r2, #0x0
 bne label_0
-mul r1, r2, r3
+cmp r2, #0x0
+bne label_1
 andeq r0, r0, r0
