3dd79e5f666c839780447faa58d798c92987baee
as12015 <as12015@ic.ac.uk>
2016-06-09 20:53:54 +0100

    Add str_util file

--- src/disassembler/dis_dp.c
+++ src/disassembler/dis_dp.c
@@ -5,6 +5,7 @@
 #include "writer.h"
 #include "../assembler/util/func_hashmap.h"
 #include "../emulator/util/shift_reg.h"
+#include "util/str_util.h"
 
 map_t opcode_map;
 
@@ -16,9 +17,6 @@ void dis_dp_set_not_rd (char*, union decoded_instr*);
 void dis_dp_set_rn (char*, union decoded_instr*);
 void dis_dp_set_rn_first (char*, union decoded_instr*);
 void dis_dp_set_not_rn (char*, union decoded_instr*);
-
-char * itoa (int value, char *result, int base);
-char* concat(char *s1, char *s2);
 
 void gen_op2(char*, union op2_gen*);
 
@@ -216,38 +214,4 @@ void dis_dp_set_not_rn(char* dest, union decoded_instr* instruction) {
 
     dest[0] = '\0';
 
-}
-
-char * itoa (int value, char *result, int base) {
-    // check that the base if valid
-    if (base < 2 || base > 36) { *result = '\0'; return result; }
-
-    char* ptr = result, *ptr1 = result, tmp_char;
-    int tmp_value;
-
-    do {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
-    } while ( value );
-
-    // Apply negative sign
-    if (tmp_value < 0) *ptr++ = '-';
-    *ptr-- = '\0';
-    while (ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr--= *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return result;
-}
-
-char* concat(char *s1, char *s2)
-{
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
-    //in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-
-    return result;
 }
---
+++ src/disassembler/util/str_util.h
@@ -0,0 +1,10 @@
+#ifndef STR_UTIL
+#define STR_UTIL
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char * itoa (int value, char *result, int base);
+char* concat(char *s1, char *s2);
+
+#endif
