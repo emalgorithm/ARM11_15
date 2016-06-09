efd466a564e82216f583723dcf38be1770f0345d
as12015 <as12015@ic.ac.uk>
2016-06-09 22:37:56 +0100

    Add updated testcase

--- test/unit/disassembler/input.s
+++ test/unit/disassembler/input.s
@@ -1,12 +1,16 @@
-add r2,r1,#2
-mov r1,#0x0F
+mov r2,#1
+lsl r2,#9
+ldr r0,=0x20200000
+str r2,[r0]
 mov r1,#1
-eor r2,r1,#0x0F
-sub r1, r2, r3, lsl r4
-mul r3,r1,r2
-cmp r1,r2
-beq foo
-mov r3,#3
-foo:
-mov r4,#4
-andeq r0, r0, r0
+lsl r1,#3
+str r1,[r0,#40]
+mov r2,#10
+loop:
+str r1,[r0,#40]
+sub r2,r2,#1
+cmp r2,#0
+str r1,[r0,#28]
+bne loop
+mul r1, r2, r3
+andeq r0,r0,r0
--- test/unit/disassembler/output.s
+++ test/unit/disassembler/output.s
@@ -1,11 +1,16 @@
-add r2, r1, #2
-mov r1, #15
-mov r1, #1
-eor r2, r1, #15
-sub r1, r2, r3, lsl r4
-mul r3, r0, r2
-cmp, r1, r2, #0
-label0:
-mov r3, #3
-mov r4, #4
+mov r2, #0x1
+mov r2, r2, #0x9
+ldr r0, =#0x20200000
+str r2, [r0, #0x0]
+mov r1, #0x1
+mov r1, r1, #0x3
+str r1, [r0, #0x28]
+mov r2, #0xA
+label_0:
+str r1, [r0, #0x28]
+sub r2, r2, #0x1
+cmp r2, #0x0
+str r1, [r0, #0x1C]
+bne label_0
+mul r1, r2, r3
 andeq r0, r0, r0
